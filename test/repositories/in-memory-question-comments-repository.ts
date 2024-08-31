f4d0b0b1f3ee867e1f6423e48db362287d868a61
Ian Hadrien <ianhadrien@gmail.com>
2024-08-31 20:21:52 -0300

    feat: fineshed crud

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
 import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
 
@@ -6,7 +7,33 @@ export class InMemoryQuestionCommentsRepository
   {
   public items: QuestionComment[] = []
 
+  async findById(id: string) {
+    const questionComments = this.items.find((item) => item.id.toString() === id);
+
+    if (!questionComments) {
+      return null
+    }
+
+    return questionComments
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
+  }
+
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment)
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id
+    )
+
+    this.items.splice(itemIndex, 1)
   }
 }
